c8df239db962acd7073162fcfe3205361ec63d1d
gaabiialves <gabialves1234.gaa@gmail.com>
2024-09-11 18:40:24 -0300

    sei mais mais

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from database import engine, get_db, MenuItem
 from scraping import scrape_menu
 from datetime import datetime
 import os
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 model.Base.metadata.create_all(bind=engine)
 
@@ -52,3 +54,23 @@ def get_menu_data(db: Session = Depends(get_db)):
         db.rollback()  # Reverte a transação em caso de erro
         print(f"Erro ao inserir dados: {e}")
         return {"error": str(e)}, status.HTTP_500_INTERNAL_SERVER_ERROR
+    
+origins = [
+ 'http://localhost:3000'
+]
+
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=origins,
+ allow_credentials=True,
+ allow_methods=['*'],
+ allow_headers=['*']
+)
+@app.get("/mensagens", response_model=List[classes.Mensagem], status_code=status.HTTP_200_OK)
+async def buscar_valores(db: Session = Depends(get_db), skip: int = 0, limit: int=100):
+ mensagens = db.query(model.Model_Mensagem).offset(skip).limit(limit).all()
+ return mensagens
+
+
+
+
